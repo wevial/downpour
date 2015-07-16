e0ccd35992433486f2529af3fa4e4e0b6ea0c5df
Sophie Rapoport <sfrapoport@users.noreply.github.com>
2015-07-16 16:57:15 -0400

    tracker_request_formatting

--- tracker.py
+++ tracker.py
@@ -1,12 +1,16 @@
 import bencode as B
 import hashlib as H
 import requests
+import urllib as U
 #import util
+#url_encode the info_hash
 
 class Tracker:
     def __init__(self, metadata):
         self.torrent = metadata.torrent
         self.tracker_url = metadata.data['announce']
+        self.params_order = ['uploaded', 'compact', 'info_hash', 'event', 
+                'downloaded', 'peer_id', 'port', 'left']
         self.params = {
             'uploaded': '0',
             'compact': '1',
@@ -17,7 +21,10 @@ class Tracker:
             'port': '6881', 
             'left': str(metadata.data['info']['length']),
         }
+    def construct_url(self):
+        params = '&'.join('%s=%s' % (key, U.quote(self.params[key])) for key in self.params_order)
+        full_url = self.tracker_url + '?' +  params
+        self.full_url = full_url
 
     def send_request(self):
-        print self.params
-        return requests.get(url=self.tracker_url, params=self.params)
+        return requests.get(url=self.full_url)
