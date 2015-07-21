b69fe37775ebef5cc93f37e0a85356221a050651
Weston Vial <wes@wesvial.com>
2015-07-21 12:16:07 -0400

    Sending handshake

--- client.py
+++ client.py
@@ -1,30 +1,43 @@
 import tracker
 import socket
 import sys
+import struct
 
 class Client:
-    def __init__(self):
-        pass
+    def __init__(self, tracker, metadata):
+        self.tracker = tracker
+        self.metadata = metadata
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def connect_to_peer(self, peer_ip, peer_port):
+    def connect_to_peer(self, peer):
+        self.socket.connect(peer)
+        print 'we are connected'
+
+    def build_handshake(self):
+        pstr = 'BitTorrent protocol'
+        info_hash = self.tracker.params['info_hash']
+        peer_id = self.tracker.params['peer_id']
+        handshake = struct.pack('B' + str(len(pstr)) + 's8x20s20s', # 8x -> reserved null bytes
+                len(pstr),
+                pstr,
+                info_hash,
+                peer_id
+                )
+        assert len(handshake) == 49 + len(pstr)
+        return handshake
+    
+    def send_handshake(self, peer):
+        self.connect_to_peer(peer)
+        handshake = self.build_handshake()
         try:
-            s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            print "Socket successfully created"
-        except socket.error as err:
-            print "Socket creation failed with error %s" %(err)
-
-        port = 80 # default port
-
-        try:
-            host_ip = peer_ip
-        except socket.gaierror:
-            # This maeans could not resolve the host
-            print "There was an issue resolving the host"
-            sys.exit()
-
-        # connecting to the server
-        s.connect((peer_ip, peer_port))
-
-        print "The socket has successfully connected to server"
-
-
+            self.socket.sendall(handshake)
+            peer_handshake = ''
+            amount_received = 0
+            amount_expected = 68 # handshake string length
+            while amount_received < amount_expected:
+                data = self.socket.recv(68)
+                peer_handshake += data
+                amount_recieved += len(data)
+        finally:
+            print peer_handshake
+            return peer_handshake
