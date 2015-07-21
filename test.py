3227cd74aa2a95ba71d95eb3f12179977d51d7c3
Weston Vial <wes@wesvial.com>
2015-07-21 16:23:53 -0400

    Fixed test.py

--- test.py
+++ test.py
@@ -15,7 +15,7 @@ t.parse_response(t.send_request())
 print 'metainfo, tracker set up.'
 peers = t.peers
 tom = P.Peer('96.126.104.219', 63529)
-print 'List of peers:\n\t', peers
+print 'List of peers (', len(peers), '):\n\t', peers
 print "Hardcoded 'peer' to Tom's ip/port =", tom
 
 print 'Setting up client...'
