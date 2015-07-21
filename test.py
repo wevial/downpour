689f7006f28d78d6db3693b18df75e89d54cffd8
Weston Vial <wes@wesvial.com>
2015-07-21 16:22:23 -0400

    Fixed test.py

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import client as C
 import metainfo as M
 import tracker as T
+import peer as P
 
 # Set things up...
 print 'Setting up metainfo, tracker...'
@@ -13,16 +14,16 @@ t.construct_url()
 t.parse_response(t.send_request())
 print 'metainfo, tracker set up.'
 peers = t.peers
-peer = ('96.126.104.219', 63529)
+tom = P.Peer('96.126.104.219', 63529)
 print 'List of peers:\n\t', peers
-print "Hardcoded 'peer' to Tom's ip/port =", peer
+print "Hardcoded 'peer' to Tom's ip/port =", tom
 
 print 'Setting up client...'
 c = C.Client(m, t)
 c.build_handshake()
 
 print 'Sending handshake...'
-handshake = c.send_handshake(peer)
+handshake = c.send_handshake(tom)
 print 'Handshake sent.'
 #handshake = c.send_handshake(peers[0]) # Frank's
 handshake = c.parse_handshake(handshake)
